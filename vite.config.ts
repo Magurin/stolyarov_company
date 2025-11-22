00454f432d4aa3369c9ffa153a6c2cbf16012a64
Magurin <stolarovigor734@gmail.com>
2025-11-23 01:43:33 +0500

    fix index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // Use relative base so built assets use relative paths. This is more
-  // resilient for GitHub Pages (project pages or custom domains).
-  base: './',
-  build: { outDir: 'docs' },
-  plugins: [react()],
+  base: '/stolyarov_company/',
+  plugins: [react()], 
 })
